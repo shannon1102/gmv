7bc72b3f4c3d775577cc6125058fa18c41b416ab
shannon1102 <vanictk62@gmail.com>
2021-08-02 14:26:14 +0700

    first-commit

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict'
+const {app} = require('./server');
+
+app.listen(5000, () => {
+    console.log(`Server listen on port 5000`)
+})
---
+++ server.js
@@ -0,0 +1,32 @@
+'use strict'
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express')
+
+const app = express();
+const backendRoute = express.Router();
+
+const indexApi = require('./api/index')
+backendRoute.use('/', indexApi)
+
+app.use('/backend', backendRoute)
+const swaggerDocument = require('./swagger.json')
+backendRoute.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+    logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} -${req.method} - ${req.ip}`)
+
+
+    res.status(err.status || 500)
+
+    res.send(err)
+})
+
+module.exports = Object.assign({},{app});
